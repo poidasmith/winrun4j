93a7122ec40e0a711836682772356046b5bba358
poida_smith <poida_smith>
2007-10-05 22:32:52 +0000

    event log

--- org.boris.winrun4j/java/src/org/boris/winrun4j/EventLog.java
+++ org.boris.winrun4j/java/src/org/boris/winrun4j/EventLog.java
@@ -4,4 +4,12 @@ package org.boris.winrun4j;
  * A mechanism for adding events.
  */
 public class EventLog {
+    public static final int SUCCESS = 0x0000;
+    public static final int ERROR = 0x0001;
+    public static final int WARNING = 0x0002;
+    public static final int INFORMATION = 0x0004;
+    public static final int AUDIT_SUCCESS = 0x0008;
+    public static final int AUDIT_FAILURE = 0x0010;
+
+    public static native void report(String source, int type, String msg);
 }
