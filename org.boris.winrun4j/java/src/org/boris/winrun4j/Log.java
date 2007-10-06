4a0811e9cb6ef0b1f32fb4326b986ad7dcef2edc
poida_smith <poida_smith>
2007-10-06 09:49:14 +0000

    logger

---
+++ org.boris.winrun4j/java/src/org/boris/winrun4j/Log.java
@@ -0,0 +1,55 @@
+package org.boris.winrun4j;
+
+/**
+ * Log to the launcher log.
+ */
+public class Log {
+    /**
+     * Info log.
+     *
+     * @param msg.
+     */
+    public static void info(String msg) {
+        log(0, msg);
+    }
+
+    /**
+     * Warning log.
+     *
+     * @param msg.
+     */
+    public static void warning(String msg) {
+        log(1, msg);
+    }
+
+    /**
+     * Error log.
+     *
+     * @param msg.
+     */
+    public static void error(String msg) {
+        log(2, msg);
+    }
+
+    /**
+     * Sets the error string, which can be retrieve via [Addin]_GetLastError() function.
+     *
+     * @param error.
+     */
+    public static native void setLastError(String error);
+
+    /**
+     * Gets the error string for the last error.
+     *
+     * @return String.
+     */
+    public static native String getLastError();
+
+    /**
+     * Internal log.
+     *
+     * @param level.
+     * @param msg.
+     */
+    private static native void log(int level, String msg);
+}
