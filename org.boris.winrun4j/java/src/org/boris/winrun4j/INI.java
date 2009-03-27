6edb37218feea604fd3089fcaf219469e32d2340
poida_smith <poida_smith>
2009-03-27 14:34:39 +0000

    *** empty log message ***

--- org.boris.winrun4j/java/src/org/boris/winrun4j/INI.java
+++ org.boris.winrun4j/java/src/org/boris/winrun4j/INI.java
@@ -11,30 +11,37 @@ package org.boris.winrun4j;
 
 import java.util.Properties;
 
-
 /**
  * Provides access to the INI file used to startup the app.
  */
-public class INI {
+public class INI
+{
+    // Known INI keys
+    public static final String MODULE_NAME = "WinRun4J:ModuleName";
+    public static final String MODULE_INI = "WinRun4J:ModuleINI";
+    public static final String MODULE_DIR = "WinRun4J:ModuleDir";
+    public static final String INI_DIR = "WinRun4J:IniDir";
+    public static final String WORKING_DIR = ":working.directory";
+
     /**
      * Gets a property from the INI file.
-     *
+     * 
      * @param key.
-     *
+     * 
      * @return String.
      */
     public static native String getProperty(String key);
 
     /**
      * Gets the keys from the INI file.
-     *
+     * 
      * @return String.
      */
     public static native String[] getPropertyKeys();
 
     /**
      * Get the set of properties as a map.
-     *
+     * 
      * @return Map.
      */
     public static Properties getProperties() {
