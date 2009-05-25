14e1f15a1f5f9d62ebb80d8bae06a17a97d00fb5
poida_smith <poida_smith>
2009-05-25 16:04:02 +0000

    *** empty log message ***

--- org.boris.winrun4j/java/src/org/boris/winrun4j/ResourceID.java
+++ org.boris.winrun4j/java/src/org/boris/winrun4j/ResourceID.java
@@ -23,4 +23,35 @@ public class ResourceID
         this.id = null;
         this.iid = iid;
     }
+
+    public boolean isIntResource() {
+        return id == null;
+    }
+
+    // Standard Resource Types
+    public static final ResourceID RT_CURSOR = new ResourceID(1);
+    public static final ResourceID RT_BITMAP = new ResourceID(2);
+    public static final ResourceID RT_ICON = new ResourceID(3);
+    public static final ResourceID RT_MENU = new ResourceID(4);
+    public static final ResourceID RT_DIALOG = new ResourceID(5);
+    public static final ResourceID RT_STRING = new ResourceID(6);
+    public static final ResourceID RT_FONTDIR = new ResourceID(7);
+    public static final ResourceID RT_FONT = new ResourceID(8);
+    public static final ResourceID RT_ACCELERATOR = new ResourceID(9);
+    public static final ResourceID RT_RCDATA = new ResourceID(10);
+    public static final ResourceID RT_MESSAGETABLE = new ResourceID(11);
+    public static final ResourceID RT_GROUP_CURSOR = new ResourceID(12);
+    public static final ResourceID RT_GROUP_ICON = new ResourceID(13);
+    public static final ResourceID RT_VERSION = new ResourceID(16);
+    public static final ResourceID RT_DLGINCLUDE = new ResourceID(17);
+    public static final ResourceID RT_PLUGPLAY = new ResourceID(19);
+    public static final ResourceID RT_VXD = new ResourceID(20);
+    public static final ResourceID RT_ANICURSOR = new ResourceID(21);
+    public static final ResourceID RT_ANIICON = new ResourceID(22);
+    public static final ResourceID RT_HTML = new ResourceID(23);
+
+    // WinRun4J types
+    public static final ResourceID RT_INI_FILE = new ResourceID(687);
+    public static final ResourceID RT_JAR_FILE = new ResourceID(688);
+    public static final ResourceID RT_SLPASH_FILE = new ResourceID(689);
 }
