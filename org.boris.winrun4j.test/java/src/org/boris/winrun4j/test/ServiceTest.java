9231fc94bce5f52d666913ca035606b2420e4ec2
poida_smith <poida_smith>
2009-05-31 08:49:45 +0000

    *** empty log message ***

--- org.boris.winrun4j.test/java/src/org/boris/winrun4j/test/ServiceTest.java
+++ org.boris.winrun4j.test/java/src/org/boris/winrun4j/test/ServiceTest.java
@@ -4,19 +4,15 @@ import org.boris.winrun4j.EventLog;
 import org.boris.winrun4j.Service;
 import org.boris.winrun4j.ServiceException;
 
-
 /**
  * A basic service.
  */
 public class ServiceTest implements Service {
     private int returnCode = 0;
-    private boolean shutdown = false;
-    
-    /* (non-Javadoc)
-     * @see org.boris.winrun4j.Service#doRequest(int)
-     */
+    private volatile boolean shutdown = false;
+
     public int doRequest(int request) throws ServiceException {
-        switch(request) {
+        switch (request) {
         case SERVICE_CONTROL_STOP:
         case SERVICE_CONTROL_SHUTDOWN:
             shutdown = true;
@@ -25,42 +21,31 @@ public class ServiceTest implements Service {
         return 0;
     }
 
-    /* (non-Javadoc)
-     * @see org.boris.winrun4j.Service#getControlsAccepted()
-     */
     public int getControlsAccepted() {
         return SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN;
     }
 
-    /* (non-Javadoc)
-     * @see org.boris.winrun4j.Service#getName()
-     */
     public String getName() {
         return "WinRun4J Test Service";
     }
 
-    /* (non-Javadoc)
-     * @see org.boris.winrun4j.Service#getDescription()
-     */
     public String getDescription() {
         return "An example service using WinRun4J.";
     }
 
-    /* (non-Javadoc)
-     * @see org.boris.winrun4j.Service#main(java.lang.String[])
-     */
     public int main(String[] args) throws ServiceException {
         int count = 0;
-        while(!shutdown) {
+        while (!shutdown) {
             try {
                 Thread.sleep(6000);
             } catch (InterruptedException e) {
             }
-            
-            if(++count % 10 == 0) 
-                EventLog.report("WinRun4J Test Service", EventLog.INFORMATION, "Ping");
+
+            if (++count % 10 == 0)
+                EventLog.report("WinRun4J Test Service", EventLog.INFORMATION,
+                        "Ping");
         }
-        
+
         return returnCode;
     }
 }
