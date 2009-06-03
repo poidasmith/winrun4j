09c81f626c0d2f612d3414ad08a78ce9779a9fce
poida_smith <poida_smith>
2009-06-03 19:08:59 +0000

    *** empty log message ***

---
+++ org.boris.winrun4j.test/java/src/org/boris/winrun4j/test/DDESingleInstanceTest.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     Peter Smith
+ *******************************************************************************/
+package org.boris.winrun4j.test;
+
+import org.boris.winrun4j.ActivationListener;
+import org.boris.winrun4j.DDE;
+import org.boris.winrun4j.Log;
+
+public class DDESingleInstanceTest
+{
+    public static void main(String[] args) throws Exception {
+    	DDE.addActivationListener(new ActivationListener() {
+			public void activate() {
+				Log.info("Activate");
+			}});
+    	ready();
+    	System.out.println("Hello world!");
+    }
+    
+    public static native void ready();
+    
+    public static void execute(String command) {
+    }
+    
+    public static void activate() {
+    	System.out.println("Activating...");
+    }
+}
