976d636a3b1618f290f64ae64d35af148f03e023
cvs2git <cvs2git>
2009-06-16 21:12:41 +0000

    This commit was manufactured by cvs2svn to create branch 'V0_3_3_BRANCH'.

--- org.boris.winrun4j.eclipse/java/src/org/boris/winrun4j/eclipse/WPreferenceInitializer.java
+++
@@ -1,9 +0,0 @@
-package org.boris.winrun4j.eclipse;
-
-import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
-
-public class WPreferenceInitializer extends AbstractPreferenceInitializer
-{
-    public void initializeDefaultPreferences() {
-    }
-}
