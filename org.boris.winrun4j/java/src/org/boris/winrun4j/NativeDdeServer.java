5b897582e73957863ac9fc478112b1dd9a2cda3e
poida_smith <poida_smith>
2009-09-29 18:47:59 +0000

    *** empty log message ***

--- org.boris.winrun4j/java/src/org/boris/winrun4j/NativeDdeServer.java
+++ org.boris.winrun4j/java/src/org/boris/winrun4j/NativeDdeServer.java
@@ -14,12 +14,23 @@ import java.util.Properties;
 
 import org.boris.winrun4j.winapi.User32;
 import org.boris.winrun4j.winapi.Hooks.POINT;
+import org.boris.winrun4j.winapi.User32.WindowProc;
+import org.boris.winrun4j.winapi.User32.WindowProcCallback;
 
-public class NativeDdeServer implements Runnable
+public class NativeDdeServer implements Runnable, WindowProc
 {
     private Thread thread;
+    private String server;
+    private String topic;
+    private String windowClass;
+    private Callback mainWndProc;
+    private long hWnd;
 
     public void initialize(String server, String topic, String windowClass) {
+        this.server = server;
+        this.topic = topic;
+        this.windowClass = windowClass == null ? "WinRun4J.DDEWndClass" : windowClass;
+        this.mainWndProc = new WindowProcCallback(this);
     }
 
     public boolean initialize(Properties ini) {
@@ -32,9 +43,71 @@ public class NativeDdeServer implements Runnable
     }
 
     public void uninitialize() {
+        mainWndProc.cleanup();
     }
 
     public void run() {
+        registerWindow(0, mainWndProc);
+        if (!registerDde()) {
+            return;
+        }
+
+        this.hWnd = CreateWindowEx(0, windowClass, "WinRun4J.DDEWindow", 0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+        runMessageLoop(0);
+    }
+
+    public int windowProc(long hWnd, int uMsg, long wParam, long lParam) {
+        return 0;
+    }
+
+    private boolean registerDde() {
+        return false;
+    }
+
+    public static final int CS_VREDRAW = 0x0001;
+    public static final int CS_HREDRAW = 0x0002;
+    public static final int CS_DBLCLKS = 0x0008;
+    public static final int CS_OWNDC = 0x0020;
+    public static final int CS_CLASSDC = 0x0040;
+    public static final int CS_PARENTDC = 0x0080;
+    public static final int CS_NOCLOSE = 0x0200;
+    public static final int CS_SAVEBITS = 0x0800;
+    public static final int CS_BYTEALIGNCLIENT = 0x1000;
+    public static final int CS_BYTEALIGNWINDOW = 0x2000;
+    public static final int CS_GLOBALCLASS = 0x4000;
+
+    private void registerWindow(long hInstance, Callback mainWndProc) {
+        WNDCLASSEX wcx = new WNDCLASSEX();
+        wcx.style = CS_BYTEALIGNCLIENT | CS_BYTEALIGNWINDOW;
+        wcx.lpfnWndProc = mainWndProc;
+        wcx.cbClsExtra = 0;
+        wcx.cbWndExtra = 30;
+        wcx.hInstance = hInstance;
+        wcx.hIcon = 0;
+        wcx.hCursor = LoadCursor(0, 32514);
+        wcx.hbrBackground = GetStockObject(1);
+        wcx.menuName = null;
+        wcx.className = windowClass;
+        wcx.hIconSm = 0;
+        RegisterClassEx(wcx);
+    }
+
+    public static long LoadCursor(long hInstance, long lpCursorName) {
+        return NativeHelper.call(User32.library, "LoadCursor", hInstance, lpCursorName);
+    }
+
+    public static long GetStockObject(int fnObject) {
+        return NativeHelper.call(User32.library, "GetStockObject", fnObject);
+    }
+
+    public static void runMessageLoop(long hWnd) {
+        long pMsg = Native.malloc(28);
+        while (GetMessage(pMsg, hWnd, 0, 0)) {
+            TranslateMessage(pMsg);
+            DispatchMessage(pMsg);
+        }
+        NativeHelper.free(pMsg);
     }
 
     public static long CreateWindowEx(int dwExStyle, String className, String windowName, int dwStyle, int x, int y,
@@ -53,18 +126,83 @@ public class NativeDdeServer implements Runnable
         MSG m = null;
         if (res != 0) {
             m = new MSG();
-            ByteBuffer bb = NativeHelper.getBuffer(ptr, 28);
-            m.hWnd = bb.getInt();
-            m.message = bb.getInt();
-            m.wParam = bb.getInt();
-            m.lParam = bb.getInt();
-            m.time = bb.getInt();
-            m.pt = new POINT();
-            m.pt.x = bb.getInt();
-            m.pt.y = bb.getInt();
+            decode(ptr, m);
         }
         NativeHelper.free(ptr);
         return m;
+    }
+
+    public static boolean GetMessage(long pMsg, long hWnd, int wMsgFilterMin, int wMsgFilterMax) {
+        return NativeHelper.call(User32.library, "GetMessage", pMsg, hWnd, wMsgFilterMin, wMsgFilterMax) != 0;
+    }
+
+    public static boolean TranslateMessage(MSG m) {
+        long ptr = Native.malloc(28);
+        encode(ptr, m);
+        boolean res = NativeHelper.call(User32.library, "TranslateMessage", ptr) != 0;
+        decode(ptr, m);
+        NativeHelper.free(ptr);
+        return res;
+    }
+
+    public static boolean TranslateMessage(long pMsg) {
+        return NativeHelper.call(User32.library, "TranslateMessage", pMsg) != 0;
+    }
+
+    public static boolean DispatchMessage(MSG m) {
+        long ptr = Native.malloc(28);
+        encode(ptr, m);
+        boolean res = NativeHelper.call(User32.library, "DispatchMessage", ptr) != 0;
+        decode(ptr, m);
+        NativeHelper.free(ptr);
+        return res;
+    }
+
+    public static boolean DispatchMessage(long pMsg) {
+        return NativeHelper.call(User32.library, "DispatchMessage", pMsg) != 0;
+    }
+
+    public static boolean RegisterClassEx(WNDCLASSEX wcx) {
+        long ptr = Native.malloc(WNDCLASSEX.SIZE);
+        long lpMenuName = NativeHelper.toNativeString(wcx.menuName, true);
+        long lpClassName = NativeHelper.toNativeString(wcx.className, true);
+        ByteBuffer bb = NativeHelper.getBuffer(ptr, WNDCLASSEX.SIZE);
+        bb.putInt(wcx.style);
+        bb.putInt((int) wcx.lpfnWndProc.getPointer());
+        bb.putInt(wcx.cbClsExtra);
+        bb.putInt(wcx.cbWndExtra);
+        bb.putInt((int) wcx.hInstance);
+        bb.putInt((int) wcx.hIcon);
+        bb.putInt((int) wcx.hCursor);
+        bb.putInt((int) wcx.hbrBackground);
+        bb.putInt((int) lpMenuName);
+        bb.putInt((int) lpClassName);
+        boolean res = NativeHelper.call(User32.library, "RegisterClassExW", ptr) != 0;
+        NativeHelper.free(ptr, lpMenuName, lpClassName);
+        return res;
+    }
+
+    public static void decode(long ptr, MSG m) {
+        ByteBuffer bb = NativeHelper.getBuffer(ptr, 28);
+        m.hWnd = bb.getInt();
+        m.message = bb.getInt();
+        m.wParam = bb.getInt();
+        m.lParam = bb.getInt();
+        m.time = bb.getInt();
+        m.pt = new POINT();
+        m.pt.x = bb.getInt();
+        m.pt.y = bb.getInt();
+    }
+
+    public static void encode(long ptr, MSG m) {
+        ByteBuffer bb = NativeHelper.getBuffer(ptr, 28);
+        bb.putInt((int) m.hWnd);
+        bb.putInt(m.message);
+        bb.putInt(m.wParam);
+        bb.putInt(m.lParam);
+        bb.putInt(m.time);
+        bb.putInt(m.pt.x);
+        bb.putInt(m.pt.y);
     }
 
     public static class MSG
@@ -75,5 +213,22 @@ public class NativeDdeServer implements Runnable
         int lParam;
         int time;
         POINT pt;
+    }
+
+    public static class WNDCLASSEX
+    {
+        public static final int SIZE = 48;
+        public int cbSize = SIZE;
+        public int style;
+        public Callback lpfnWndProc;
+        public int cbClsExtra;
+        public int cbWndExtra;
+        public long hInstance;
+        public long hIcon;
+        public long hCursor;
+        public long hbrBackground;
+        public String menuName;
+        public String className;
+        public long hIconSm;
     }
 }
--- org.boris.winrun4j/java/src/org/boris/winrun4j/winapi/User32.java
+++ org.boris.winrun4j/java/src/org/boris/winrun4j/winapi/User32.java
@@ -9,9 +9,33 @@
  *******************************************************************************/
 package org.boris.winrun4j.winapi;
 
+import java.nio.ByteBuffer;
+
+import org.boris.winrun4j.Callback;
 import org.boris.winrun4j.Native;
+import org.boris.winrun4j.NativeHelper;
 
 public class User32
 {
     public static final long library = Native.loadLibrary("user32");
+
+    public interface WindowProc
+    {
+        public int windowProc(long hWnd, int uMsg, long wParam, long lParam);
+    }
+
+    public static class WindowProcCallback extends Callback
+    {
+        private WindowProc callback;
+
+        public WindowProcCallback(WindowProc callback) {
+            this.callback = callback;
+        }
+
+        protected int callback(int stack) {
+            ByteBuffer bb = NativeHelper.getBuffer(stack + 8, 16);
+            return callback.windowProc(bb.getInt(), bb.getInt(), bb.getInt(), bb.getInt());
+        }
+    }
+
 }
