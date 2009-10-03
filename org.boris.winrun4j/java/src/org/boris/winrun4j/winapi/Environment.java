6cc6878f5ba036e11c3b61c8206ed0568a71bda0
poida_smith <poida_smith>
2009-10-03 11:26:56 +0000

    *** empty log message ***

--- org.boris.winrun4j/java/src/org/boris/winrun4j/winapi/Environment.java
+++ org.boris.winrun4j/java/src/org/boris/winrun4j/winapi/Environment.java
@@ -16,13 +16,12 @@ public class Environment
     public static String ExpandEnvironmentString(String var) {
         if (var == null)
             return null;
-        long str = NativeHelper.toNativeString(var, false);
+        long str = NativeHelper.toNativeString(var, true);
         long buf = Native.malloc(4096);
-        long res = NativeHelper.call(library, "ExpandEnvStrings", str, buf,
-                4096);
+        long res = NativeHelper.call(library, "ExpandEnvironmentStringsW", str, buf, 4096);
         String rs = null;
         if (res > 0 && res <= 4096) {
-            rs = NativeHelper.getString(buf, 4096, false);
+            rs = NativeHelper.getString(buf, 4096, true);
         }
         Native.free(str);
         Native.free(buf);
@@ -30,8 +29,8 @@ public class Environment
     }
 
     public static String[] GetCommandLine() {
-        long res = NativeHelper.call(library, "GetCommandLine");
-        String s = NativeHelper.getString(res, 1024, false);
+        long res = NativeHelper.call(library, "GetCommandLineW");
+        String s = NativeHelper.getString(res, 1024, true);
         boolean inQuote = false;
         ArrayList args = new ArrayList();
         StringBuffer sb = new StringBuffer();
@@ -59,31 +58,31 @@ public class Environment
     }
 
     public static Properties GetEnvironmentVariables() {
-        long buf = NativeHelper.call(library, "GetEnvStrings");
-        ByteBuffer bb = Native.fromPointer(buf, 32767);
+        long buf = NativeHelper.call(library, "GetEnvironmentStringsW");
+        ByteBuffer bb = NativeHelper.getBuffer(buf, 32767);
         Properties p = new Properties();
         while (true) {
-            String s = NativeHelper.getString(bb, false);
+            String s = NativeHelper.getString(bb, true);
             if (s == null || s.length() == 0)
                 break;
             int idx = s.indexOf('=');
             p.put(s.substring(0, idx), s.substring(idx + 1));
         }
-        NativeHelper.call(library, "FreeEnvStrings", buf);
+        NativeHelper.call(library, "FreeEnvironmentStringsW", buf);
         return p;
     }
 
     public static String GetEnvironmentVariable(String var) {
         if (var == null)
             return null;
-        long buf = NativeHelper.toNativeString(var, false);
+        long buf = NativeHelper.toNativeString(var, true);
         long rbuf = Native.malloc(4096);
-        long res = NativeHelper.call(library, "GetEnvVar", buf, rbuf, 4096);
+        long res = NativeHelper.call(library, "GetEnvironmentVariableW", buf, rbuf, 4096);
         if (res == 0)
             return null;
         if (res > 4096)
             return null;
-        String str = NativeHelper.getString(rbuf, 4096, false);
+        String str = NativeHelper.getString(rbuf, 4096, true);
         Native.free(buf);
         Native.free(rbuf);
         return str;
@@ -92,12 +91,12 @@ public class Environment
     public static File[] GetLogicalDrives() {
         int len = 1024;
         long buf = Native.malloc(len);
-        long res = NativeHelper.call(library, "GetLogicalDrive", len, buf);
-        ByteBuffer bb = Native.fromPointer(buf, res + 1);
+        long res = NativeHelper.call(library, "GetLogicalDriveStringsW", len, buf);
+        ByteBuffer bb = NativeHelper.getBuffer(buf, (int) (res + 1) << 1);
         ArrayList drives = new ArrayList();
         StringBuffer sb = new StringBuffer();
         while (true) {
-            char c = (char) bb.get();
+            char c = (char) bb.getChar();
             if (c == 0) {
                 if (sb.length() == 0) {
                     break;
@@ -115,12 +114,11 @@ public class Environment
 
     public static OSVERSIONINFOEX GetVersionEx() {
         long pOs = Native.malloc(156);
-        ByteBuffer b = Native.fromPointer(pOs, 156).order(
-                ByteOrder.LITTLE_ENDIAN);
+        ByteBuffer b = Native.fromPointer(pOs, 156).order(ByteOrder.LITTLE_ENDIAN);
         NativeHelper.zeroMemory(b);
         b.rewind();
         b.putInt(156); // set dwOSVersionInfoSize;
-        long res = NativeHelper.call(library, "GetVersionEx", pOs);
+        long res = NativeHelper.call(library, "GetVersionExA", pOs);
         if (res == 0) {
             Native.free(pOs);
             return null;
