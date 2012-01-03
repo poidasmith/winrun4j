3f9b7c564ee03ee65f513c5ea8a78d7fd0dcbd7a
poidasmith <poida.smith@gmail.com>
2012-01-03 21:07:57 +0800

    improve launcher for deploy regression testing

--- org.boris.winrun4j.test/java/src/org/boris/winrun4j/test/framework/JUnitLauncher.java
+++ org.boris.winrun4j.test/java/src/org/boris/winrun4j/test/framework/JUnitLauncher.java
@@ -10,25 +10,63 @@
 package org.boris.winrun4j.test.framework;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FilenameFilter;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.junit.Test;
 
 public class JUnitLauncher
 {
     public static void main(String[] args) throws Throwable {
-        File[] classes = IO.findFiles(new File("bin"), new FilenameFilter() {
+        String[] cpElements = System.getProperty("java.class.path").split(";");
+        List<String> classes = new ArrayList<String>();
+        for(String elem : cpElements) {
+            File f = new File(elem);
+            if(!f.exists())
+                continue;
+            if(f.isDirectory())
+                findClassesInDir(f, classes);
+            else 
+                findClassesInZip(f, classes);
+        }
+        for(String clazz : classes) {
+            if(clazz.contains("winrun4j.test"))
+                test(clazz);
+        }
+    }
+
+    private static void findClassesInZip(File f, List<String> classes) throws Throwable {
+        ZipInputStream zis = new ZipInputStream(new FileInputStream(f));
+        ZipEntry ze = null;
+        
+        while((ze = zis.getNextEntry()) != null) {
+            String s = ze.getName();
+            if(s.endsWith(".class")) {
+                s = s.substring(0, s.length() - 6);
+                s = s.replaceAll("/", ".");
+                classes.add(s);
+            }
+        }
+    }
+
+    private static void findClassesInDir(File dir, List<String> classes) throws Throwable {
+        File[] files = IO.findFiles(dir, new FilenameFilter() {
             public boolean accept(File dir, String name) {
                 return name.endsWith(".class");
             }
         });
-        for (File f : classes) {
-            String clazz = f.toString().substring(4);
-            clazz = clazz.substring(0, clazz.length() - 6).replace('\\', '.');
-            test(clazz);
+        for(File f : files) {
+            String clazz = f.toString().substring(dir.toString().length() + 1);
+            clazz = clazz.substring(0, clazz.length() - 6);
+            clazz = clazz.replaceAll("\\\\", ".");
+            classes.add(clazz);
         }
     }
 
