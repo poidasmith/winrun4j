c9ca5e8b7de754bc2488caad1b7e5e8615c899ab
poidasmith <poida.smith@gmail.com>
2012-01-16 20:46:57 +0800

    test large INI files

--- org.boris.winrun4j.test/java/src/org/boris/winrun4j/test/unit/INITest.java
+++ org.boris.winrun4j.test/java/src/org/boris/winrun4j/test/unit/INITest.java
@@ -9,10 +9,18 @@
  *******************************************************************************/
 package org.boris.winrun4j.test.unit;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.StringReader;
+
 import org.boris.winrun4j.Launcher;
+import org.boris.winrun4j.test.framework.IO;
 import org.boris.winrun4j.test.framework.PrintEnvironment;
+import org.boris.winrun4j.test.framework.ProcessResult;
+import org.boris.winrun4j.test.framework.RCEDIT;
 import org.boris.winrun4j.test.framework.TestHelper;
 import org.junit.Test;
 
@@ -33,7 +41,26 @@ public class INITest
         assertTrue(result.contains("WinRun4J:module.ini="));
     }
 
+    /**
+     * Bounds checking large INIs
+     */
+    public void testLargeINI() throws Exception {
+        Launcher l = TestHelper.launcher().create();
+        for (int i = 0; i < 100; i++)
+            l.vmarg("-Dx=testing.large.ini");
+        l.main(PrintEnvironment.class);
+        String ini = l.toString();
+        File exe = l.getLauncher();
+        File inif = File.createTempFile("ini", ".ini");
+        IO.copy(new StringReader(ini), new FileWriter(inif), true);
+        RCEDIT.setINI(exe, inif);
+        String result = RCEDIT.printINI(exe);
+        assertTrue(result.contains("vmarg.101"));
+        ProcessResult pr = TestHelper.start(l).waitFor();
+        assertEquals(pr.exitValue(), 0);
+    }
+
     public static void main(String[] args) throws Exception {
-        new INITest().testINI();
+        new INITest().testLargeINI();
     }
 }
